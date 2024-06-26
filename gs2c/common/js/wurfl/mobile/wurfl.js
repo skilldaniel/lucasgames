caff2be2f1ee7713d3aa9a7ebf49b109f7853421
passiondev <153737110+skilldaniel@users.noreply.github.com>
2024-06-27 02:03:46 +0800

    initial

---
+++ gs2c/common/js/wurfl/mobile/wurfl.js
@@ -0,0 +1,35 @@
+// This file is generated dynamically per-request and cannot be used directly.
+// To use WURFL.js, please see the documentation at https://web.wurfl.io/#wurfl-js
+//
+// Copyright 2021 - ScientiaMobile, Inc., Reston, VA
+// WURFL Device Detection
+// Terms of service:
+// https://www.scientiamobile.com/terms-service-wurfl-js-imageengine-be/
+
+var WURFL = {
+    "advertised_browser": "Chrome Mobile",
+    "advertised_browser_version": "91.0.4472.124",
+    "advertised_device_os": "Android",
+    "advertised_device_os_version": "5.0",
+    "brand_name": "Samsung",
+    "complete_device_name": "Samsung SM-G900P (Galaxy S5)",
+    "form_factor": "Smartphone",
+    "is_app_webview": false,
+    "is_full_desktop": false,
+    "is_mobile": true,
+    "is_robot": false,
+    "is_smartphone": true,
+    "is_smarttv": false,
+    "is_tablet": false,
+    "manufacturer_name": "",
+    "marketing_name": "Galaxy S5",
+    "max_image_height": 640,
+    "max_image_width": 360,
+    "model_name": "SM-G900P",
+    "physical_screen_height": 113,
+    "physical_screen_width": 64,
+    "pointing_method": "touchscreen",
+    "resolution_height": 1920,
+    "resolution_width": 1080
+};
+document.dispatchEvent(new Event("WurflJSDetectionComplete", {bubbles: true}));
